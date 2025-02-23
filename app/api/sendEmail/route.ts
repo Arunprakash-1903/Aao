b3e5af1640c9e3dd21aa41dbb2675faa394989d0
Arunprakash-1903 <ap9396313@gmail.com>
2025-02-23 15:50:05 +0530

    path change in email

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -1,6 +1,8 @@
 import { sendEmail } from "@lib/mail"
 import { NextResponse } from "next/server";
 import prisma from "prisma/prisma";
+import path from 'path';
+
 import fs from 'fs'
 export async function POST(request:Request){
 const {tomail}=await request.json()
@@ -25,8 +27,9 @@ const user =await prisma.user.findUnique({
       }
     },
   });
-if(fs.existsSync(`app\\(uploads)\\${tomail}\\${user.profileDocument}`))
-    sendEmail(tomail,user.profileDocument,`app\\(uploads)\\${tomail}\\${user.profileDocument}`)
+  const filePath = path.join("app", "(uploads)", tomail, user.profileDocument);
+if(fs.existsSync(filePath))
+    sendEmail(tomail,user.profileDocument,filePath)
 else
     return NextResponse.json({message:"no document found"},{status:200})
 return NextResponse.json({message:"sent"})
