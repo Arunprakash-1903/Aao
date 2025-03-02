1dd132f7850f30b97b78b4c69a9d698077745b1e
Arunprakash-1903 <ap9396313@gmail.com>
2025-03-02 18:06:15 +0530

    webi hook update

--- app/api/razorpay-webhook/route.ts
+++ app/api/razorpay-webhook/route.ts
@@ -1,11 +1,53 @@
+import { authOptions } from "@lib/auth";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
-
+import prisma from 'prisma/prisma';
 export async function POST(req: Request) {
   const body = await req.json();
   console.log("Webhook event:", body);
+  const session=await getServerSession(authOptions)
 
+      //const user=await prisma.user.findUnique({where: { email:session?.user?.email ||"" },});
   if (body.event === "subscription.activated") {
     console.log("Subscription activated for:", body.payload.subscription.entity.customer_id);
+console.log(session);
+
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email:session?.user?.email,
+      },
+     
+    })
+
+
+    await prisma.user.update({
+        where: {
+          email:session?.user?.email,
+        },
+        data: {
+          subcribed:true,
+        },
+      })
+    console.log("subcribed",user);
+    
+    
+        
+          
+          
+          
+          const response = await fetch(`${process.env.NEXTAUTH_URL}/api/purchase`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              // userId:3,
+              // courseId: 1,
+              email:session?.user?.email,
+              id:"1"
+            }),
+          });
   }
 
   return NextResponse.json({ message: "Webhook received" });
